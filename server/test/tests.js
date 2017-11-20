ad0558951feb00dd238e947033884e0e2e922d2a
efosa <efosaokpugie@gmail.com>
2017-11-20 08:52:50 +0100

    write simple test for user signup route

--- server/test/tests.js
+++ server/test/tests.js
@@ -2,8 +2,6 @@ import request from 'supertest';
 import chai from 'chai';
 import app from '../app';
 
-const { expect } = chai;
-
 describe('test-cases for api routes', () => {
   describe('GET /', () => {
     it('responds with a 200 and welcome message in json', (done) => {
@@ -22,6 +20,27 @@ describe('test-cases for api routes', () => {
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200, { message: 'Welcome to the events-manager Api' }, done);
+    });
+  });
+  
+  describe('POST /api/v1/users', () => {
+    it('responds with a 200 and welcome message in json', (done) => {
+      const userCredentials = {
+        firstname: 'ororo',
+        lastname: 'ronaldo',
+        email: 'lionelmessi@barca.com',
+        password: 'thegreatest',
+        confirmpassword: 'thegreatest',
+      };
+      request(app)
+        .post('/api/v1/users')
+        .send(userCredentials)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(201, done)
+        .expect((res) => {
+          console.log(res.body);
+        });
     });
   });
 });
