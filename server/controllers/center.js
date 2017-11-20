ef519a7fca3a18c566f5bcaf1afcc10b633646c6
efosa <efosaokpugie@gmail.com>
2017-11-20 12:37:40 +0100

    write logic to handle modify a recipe endpoint

--- server/controllers/center.js
+++ server/controllers/center.js
@@ -22,6 +22,36 @@ class Center {
       .then(res.status(200).send({ message: 'You have successfully added a center' }))
       .catch(res.status(500).send({ error: 'Oops, an error occurred' }));
   }
+
+  static modifyCenter(req, res) {
+    const {
+      isAdmin,
+      userId,
+    } = req.decoded;
+    if (!isAdmin) {+
+      return res.status(400).send({ error: 'You are not authorized to perform this action' });
+    }
+    return Centers
+      .findById(req.params.centerId)
+      .then((center) => {
+        if (!center) {
+          return res.status(400).send({ error: 'center not found!' });
+        }
+        if (center && center.centerOwner !== userId) {
+          return res.status(400).send({ error: 'You cannot modify a center added by another user' });
+        }
+        center.updateAttributes({
+          name: req.body.name || center.name,
+          type: req.body.type || center.type,
+          location: req.body.location || center.location,
+          address: req.body.address || center.address,
+          mobileNumber: req.body.mobileNumber || center.mobileNumber,
+          imageUrl: req.body.imageUrl || center.imageUrl,
+        });
+        return res.status(200).send({ message: 'You have successfully modified the center' });
+      })
+      .catch(res.status(500).send({ error: 'oops an error occurred' }));
+  }
 }
 
 export default Center;
