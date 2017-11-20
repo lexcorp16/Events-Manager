e5355dc65288e6a721710f324d7482244cc278ea
efosa <efosaokpugie@gmail.com>
2017-11-20 01:02:13 +0100

    edit user model file to remove bug

--- server/db/models/users.js
+++ server/db/models/users.js
@@ -1,17 +1,42 @@
-'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var Users = sequelize.define('Users', {
-    firstname: DataTypes.STRING,
-    lastname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN,
-    password: DataTypes.STRING
+export default (sequelize, DataTypes) => {
+  const Users = sequelize.define('Users', {
+    id: {
+      allowNull: false,
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    isAdmin: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   }, {
     classMethods: {
-      associate: function(models) {
+      associate: (models) => {
         // associations can be defined here
-      }
-    }
+      },
+    },
   });
+  Users.associate = (models) => {
+    Users.hasMany(models.Event, { as: 'events', foreignKey: 'eventOwner' });
+    Users.hasMany(models.Center, { as: 'owner', foreignKey: 'centerOwner' });
+  };
   return Users;
 };
