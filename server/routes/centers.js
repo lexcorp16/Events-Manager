1d8600a5e9dbf72207de1867cf8f2fbe33f09f3c
efosa <efosaokpugie@gmail.com>
2017-11-20 12:23:56 +0100

    added route handler for add event api endpoint

--- server/routes/centers.js
+++ server/routes/centers.js
@@ -1,5 +1,10 @@
+import centerControllers from '../controllers/center';
+import auth from '../middlewares/auth';
+
 export default (app) => {
   app.get('/api', (req, res) => {
     res.status(200).send({ message: 'Welcome to the events-manager Api' });
   });
+  app.post('/api/v1/centers', auth, centerControllers.addCenter);
+  app.put('/api/v1/centers/:centerId', auth, centerControllers.modifyCenter);
 };
