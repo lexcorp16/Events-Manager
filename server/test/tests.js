9db93facef56782cbbe7289161f9911db0ff116f
efosa <efosaokpugie@gmail.com>
2017-11-20 10:12:19 +0100

    write simple tests for signin api endpoint

--- server/test/tests.js
+++ server/test/tests.js
@@ -2,7 +2,10 @@ import request from 'supertest';
 import chai from 'chai';
 import app from '../app';
 
+const { expect } = chai;
+
 describe('test-cases for api routes', () => {
+  let token;
   describe('GET /', () => {
     it('responds with a 200 and welcome message in json', (done) => {
       request(app)
@@ -22,9 +25,9 @@ describe('test-cases for api routes', () => {
         .expect(200, { message: 'Welcome to the events-manager Api' }, done);
     });
   });
-  
+
   describe('POST /api/v1/users', () => {
-    it('responds with a 200 and welcome message in json', (done) => {
+    it('creates a new User an responds with 201', (done) => {
       const userCredentials = {
         firstname: 'ororo',
         lastname: 'ronaldo',
@@ -40,6 +43,26 @@ describe('test-cases for api routes', () => {
         .expect(201, done)
         .expect((res) => {
           console.log(res.body);
+        });
+    });
+  });
+
+  describe('POST /api/v1/users', () => {
+    it('responds with a 200 and signs in a user', (done) => {
+      const userCredentials = {
+        email: 'lionelmessi@barca.com',
+        password: 'thegreatest',
+      };
+      request(app)
+        .post('/api/v1/users/signin')
+        .send(userCredentials)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done)
+        .expect((res) => {
+          token = res.body.token;
+          expect(typeof res.body.token).to.be.a('string');
+          console.log(`TOKEN HERE ${token}`);
         });
     });
   });
