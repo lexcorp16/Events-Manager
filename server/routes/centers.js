fc0057f304d2f32eb3d6aef066d84a832935b5a9
efosa <efosaokpugie@gmail.com>
2017-11-22 15:04:05 +0100

    add route handler to handle get All center api endpoint

--- server/routes/centers.js
+++ server/routes/centers.js
@@ -6,6 +6,6 @@ export default (app) => {
     res.status(200).send({ message: 'Welcome to the events-manager Api' });
   });
   app.post('/api/v1/centers', auth, centerControllers.addCenter);
-  app.put('/api/v1/centers:centerId', auth, centerControllers.modifyCenter);
+  app.put('/api/v1/centers/:centerId', auth, centerControllers.modifyCenter);
   app.get('/api/v1/centers', centerControllers.getAllCenters);
 };
