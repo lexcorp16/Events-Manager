3f706423e9ec8ab9080aeee43fbe8ddaa4572364
efosa <efosaokpugie@gmail.com>
2017-11-23 20:18:50 +0100

    heroku deployment version

---
+++ dist/db/migrations/20171119115918-create-center.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  up: function up(queryInterface, Sequelize) {
+    return queryInterface.createTable('Centers', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      type: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      location: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      address: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      UserId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      },
+      mobileNumber: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function down(queryInterface) {
+    return queryInterface.dropTable('Centers');
+  }
+};
