c63c22c7166f2f15a0177507ee28d12814d6c744
efosa <efosaokpugie@gmail.com>
2017-11-27 21:51:30 +0100

    import navbar and signup templates into app.js

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,12 +1,22 @@
-const React = require('react');
-const { Component } = require('react');
-
+import React, { Component } from 'react';
+import Footer from '../components/footer';
+import SignupBody from '../components/signupbody';
+import Navbar from '../components/navbar';
 /**
 * @Center, class containing all methods that
 * handle center related api endpoint
 */
 export default class App extends Component {
   render() {
-    return <p>This is my new react app</p>;
+    return (
+      <div>
+        <div className='container-fluid'>
+        <Navbar />
+        <SignupBody />
+        </div>
+
+        <Footer />
+      </div>
+    );
   }
 }
