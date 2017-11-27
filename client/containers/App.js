bf02feb3aefdb238b625209418fce3882c37e897
efosa <efosaokpugie@gmail.com>
2017-11-27 13:47:00 +0100

    create react App file

---
+++ client/containers/App.js
@@ -0,0 +1,12 @@
+const React = require('react');
+const { Component } = require('react');
+
+/**
+* @Center, class containing all methods that
+* handle center related api endpoint
+*/
+export default class App extends Component {
+  render() {
+    return <p>This is my new react app</p>;
+  }
+}
