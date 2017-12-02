8c65a1491035cc6157e857a3a0f1f1fdf5b2da4d
efosa <efosaokpugie@gmail.com>
2017-12-02 17:30:42 +0100

    create event action file to dispatch action type

--- client/actions/eventActions.js
+++ client/actions/eventActions.js
@@ -1,5 +1,23 @@
 import axios from 'axios';
+// import { browserHistory } from 'react-router';
 
-export function userSigninRequest(allCenters) {
-  return dispatch => axios.get('http://localhost:1234/api/v1/centers', allCenters);
-}
+const addEvent = (eventDetails) => {
+  return (dispatch) => {
+    dispatch({ type: 'ADD_EVENT' });
+    console.log(localStorage.getItem('x-access-token'));
+    axios({
+      method: 'POST',
+      url: 'http://localhost:1234/api/v1/events',
+      headers: { 'x-access-token': localStorage.getItem('x-access-token')},
+      data: eventDetails,
+    })
+      .then((res) => {
+        dispatch({ type: 'ADD_EVENT_RESOLVED', payload: res.data });
+      })
+      .catch((err) => {
+        dispatch({ type: 'ADD_EVENT_REJECTED', payload: err.response.data });
+      });
+  };
+};
+
+export default addEvent;
