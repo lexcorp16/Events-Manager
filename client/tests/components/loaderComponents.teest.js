e9c778fde2e24149674ed99e4b53c3bce05edd1f
Efosaok <efosaokpugie@gmail.com>
2018-06-13 22:33:40 +0100

    --amend

---
+++ client/tests/components/loaderComponents.teest.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expect from 'expect';
+import { UploadProgressbar, LoadingIcon, SpinnerProgressBar, LargeLoadingIcon } from '../../components/utils/LoaderComponents';
+
+const firstWrapper = shallow(<UploadProgressbar />);
+const secondWrapper = shallow(<LoadingIcon />);
+const thirdWrapper = shallow(<SpinnerProgressBar />);
+const fourthWrapper = shallow(<LargeLoadingIcon />);
+
+describe('unauthenticated HOC', () => {
+  it('mounts centercard component', () => {
+    expect(firstWrapper.exists()).toBe(true);
+    expect(secondWrapper.exists()).toBe(true);
+  });
+});
